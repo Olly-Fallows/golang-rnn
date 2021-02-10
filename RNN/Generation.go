acc457ea3aeead96ce00b328a8459541fc725d83
Olly-Fallows <ollyfallows@outlook.com>
2021-02-10 12:13:49 +0000

    Added basic file stuff

--- RNN/Generation.go
+++ RNN/Generation.go
@@ -3,6 +3,8 @@ package rnn
 import (
   "math/rand"
   "time"
+  "os"
+  "encoding/csv"
 )
 
 func MakeRNN(layers []int) (*Network, error) {
@@ -96,5 +98,92 @@ func MakeNormRNN(layers []int) (*Network, error) {
   return &Network{
     Layers: l,
     Depth: d,
+  }, nil
+}
+
+func LoadRNN(path string) (*Network, error) {
+  var m *rnn.Network
+  // Load template file
+  f, err := os.Open(path)
+  if err != nil {
+    fmt.Printf("[E] Failed to read template file with error: %v\n", err)
+    return nil, err
+  }
+  defer f.Close()
+
+  lines, err := csv.NewReader(f).ReadAll()
+  if err != nil {
+    fmt.Printf("[E] Failed to parse template file with error: %v\n", err)
+    return nil, err
+  } else if len(lines) == 0 {
+    fmt.Printf("[E] Template was empty.\n")
+    return nil, err
+  }
+
+  layers := make([]Layer, len(lines[0]))
+  for a, l := range lines[0] {
+    size, err := strconv.Atoi(l)
+    if err != nil {
+      fmt.Printf("[E] Failed to convert \"%v\" to int.\n", l)
+      return nil, err
+    }
+    layers[a].Size = size
+    layers[a].Biases = make([]float64, size)
+    layers[a].Values = make([]float64, size)
+    layers[a].Alphas = make([][]float64, size)
+    for b=0; b<size; b++ {
+      layers[a].Alphas = make([]float64, size)
+    }
+    if a > 0 {
+      layers[a].Weights = make([][]float64, size)
+      for b=0; b<layers[a-1].Size; b++ {
+        layers[a].Weights = make([]float64, layers[a-1].Size)
+      }
+    }
+  }
+
+  l := 0
+  stage := 0
+  for a, line := range lines[1:] {
+    if stage == 0 {
+      // Biases
+      for b, d := range line {
+        val, err := strconv.Atoi(l)
+        if err != nil {
+          fmt.Printf("[E] Failed to convert \"%v\" to int.\n", l)
+          return nil, err
+        }
+        layers[l].Biases[b] = val
+      }
+    } else if stage < layers[l].Size+1 {
+      // Alphas
+      for b, d := range line {
+        val, err := strconv.Atoi(l)
+        if err != nil {
+          fmt.Printf("[E] Failed to convert \"%v\" to int.\n", l)
+          return nil, err
+        }
+        layers[l].Alphas[stage-1][b] = val
+      }
+    } else if a > 0 && stage < (layers[l].Size*2)+1 {
+      // Weights
+      for b, d := range line {
+        val, err := strconv.Atoi(l)
+        if err != nil {
+          fmt.Printf("[E] Failed to convert \"%v\" to int.\n", l)
+          return nil, err
+        }
+        layers[l].Weights[stage-(layers[l].Size+1)][b] = val
+      }
+    } else {
+      stage = -1
+      l += 1
+    }
+    stage += 1
+  }
+  
+  return &Network {
+    Layers: layers,
+    Depth: len(layers),
   }, nil
 }
--- RNN/Structures.go
+++ RNN/Structures.go
@@ -2,6 +2,7 @@ package rnn
 
 import (
   "github.com/Olly-Fallows/golang-rnn/Logging"
+  "fmt"
 )
 
 type Layer struct {
@@ -26,4 +27,43 @@ func (n *Network) Clean() error {
     }
   }
   return nil
+}
+
+
+func (n *Network) ToCSV() (string) {
+  str := ""
+  for a, l := n.Layers {
+    if a > 0 {
+      str = fmt.Sprintf("%v%v", str, ",")
+    }
+    str = fmt.Sprintf("%v%v", str, l.Size)
+  }
+  for a, l := n.Layers {
+    str = fmt.Sprintf("%v%v", str, "\n")
+    for b, vals := l.Biases {
+      if b > 0 {
+        str = fmt.Sprintf("%v%v", str, ",")
+      }
+      str = fmt.Sprintf("%v%v", str, vals[b])
+    }
+    for b, vals := l.Alphas {
+      str = fmt.Sprintf("%v%v", str, "\n")
+      for c, v := vals {
+        if c > 0 {
+          str = fmt.Sprintf("%v%v", str, ",")
+        }
+        str = fmt.Sprintf("%v%v", str, v[c])
+      }
+    }
+    for b, vals := l.Weights {
+      str = fmt.Sprintf("%v%v", str, "\n")
+      for c, v := vals {
+        if c > 0 {
+          str = fmt.Sprintf("%v%v", str, ",")
+        }
+        str = fmt.Sprintf("%v%v", str, v[c])
+      }
+    }
+  }
+  return str
 }
